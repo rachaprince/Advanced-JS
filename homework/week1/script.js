ce0920a7ae0cdfe57f4f1747c4525f968c1ee2eb
Rachel Prince <rachel.prince@yale.edu>
2014-09-23 08:03:03 -0400

    not able to use get request for one of the APIs, hoping to figure that out this afternoon!

--- homework/week1/script.js
+++ homework/week1/script.js
@@ -7,14 +7,43 @@ $(document).ready(function() {
     for(var i in stories) {
       story = stories[i].data;
       var elem = $("<li></li>");
+      var tit = story.title;
+      var permalink = story.permalink;
+      var upvote = story.ups;
       // Your code here:
       // In the data in the Javascript object 'story',
       // find the title, permalink, and upvote count
       // Then create HTML elements with jQuery (like in line 9)
       // and append them to the #list element. 
       $("#list").append(elem);
+      $("#list").append(tit);
+      $("#list").append(permalink);
+      $("#list").append(upvote);
       // Look at the JS console in Chrome to see what story looks like
       console.log(story);
     }
   })
+  $("#refresh-button").click(function() {
+    location.reload(true);
+  });
+     $.get("https://petitions.whitehouse.gov/v1/petitions", function(response) {
+      var petitions= response.data.children;
+      for (var i in petitions) {
+        petition = petitions[i];
+        var elem = $("<li></li>");
+      var tit = petition.title;
+      var issues = petitions.issues.name;
+      var signature = petions.signatureCount;
+      // Your code here:
+      // In the data in the Javascript object 'story',
+      // find the title, permalink, and upvote count
+      // Then create HTML elements with jQuery (like in line 9)
+      // and append them to the #list element. 
+      $("#list").append(elem);
+      $("#list").append(tit);
+      $("#list").append(issues);
+      $("#list").append(signature);
+
+      }
+     })
 });
